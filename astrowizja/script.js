48296f215a6e764861e4627fb880c11ff86799b1
szuuuu <sz@toyspring.com>
2017-01-20 12:27:16 +0100

    podswietlanie wybranych zdjec, mozliwosc ukrycia niewybranych, dodany link do astropolis

--- astrowizja/script.js
+++ astrowizja/script.js
@@ -20,23 +20,32 @@ function klikZdjecia(event)
     img.src=href;
     var nazwa=event.currentTarget.getElementsByTagName('span')[0].textContent;
     document.getElementById("nazwa").innerHTML=nazwa+"...";
-    if (currently_selected) currently_selected.className="";
+    if (currently_selected) currently_selected.classList.remove("selected");
     currently_selected=a.parentElement;
-    currently_selected.className="selected";
+    currently_selected.classList.add("selected");
     setTimeout(function(){wczytuje(nazwa);},500);
     var info=document.getElementById("wybierzcos");
     if (info) info.remove();
     return false;
 }
 
+function klikCheckbox(event)
+{
+	var ch=event.currentTarget;
+	if (ch.checked)
+		ch.parentElement.classList.add("checked");
+	else
+		ch.parentElement.classList.remove("checked");
+}
+
 function wypelnijListeZdjec()
 {
-    lista=document.getElementById("lista");
+    lista=document.getElementById("miniaturki");
     var html="";
     for(var i=0;i<zdjecia.length;i++)
     {
 	var z=zdjecia[i];
-	html+="<div><input type=\"checkbox\"><a href=\""+z[2]+"\" onclick=\"return klikZdjecia(event)\"><span>"+z[0]+"</span><img src=\""+z[1]+"\"></a></div>\n";
+	html+="<div><input type=\"checkbox\" onchange=\"klikCheckbox(event)\"><a href=\""+z[2]+"\" onclick=\"return klikZdjecia(event)\"><span>"+z[0]+"</span><img src=\""+z[1]+"\"></a></div>\n";
     }
     lista.innerHTML=html;
 }
@@ -64,4 +73,17 @@ function zdjecieZoom11()
 {
     var img=document.getElementById("obraz");
     img.width=img.naturalWidth;
+}
+
+function ukryjNiewybrane(event)
+{
+    var checks=document.getElementById("miniaturki").getElementsByTagName('input');
+    for(var i=0;i<checks.length;i++)
+		if (event.currentTarget.checked)
+			{
+	    	if (!checks[i].checked)
+    			checks[i].parentElement.classList.add("hidden");
+			}
+		else
+	    	checks[i].parentElement.classList.remove("hidden");
 }
